079f69e544a44a935fb3a1a34786c6b38279c2b5
darshansavaliya7373 <darshansavaliya7373@gmail.com>
2023-12-31 15:04:17 +0530

    donne

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require("mongoose")
 var db = mongoose.connection
-mongoose.connect("mongodb://127.0.0.1/skd")
-// mongoose.connect("mongodb+srv://morsy:morsy@ds.6e7bjag.mongodb.net/skd")
+// mongoose.connect("mongodb://127.0.0.1/skd")
+mongoose.connect("mongodb+srv://morsy:morsy@ds.6e7bjag.mongodb.net/skd")
 db.once('open',(err)=>{
 
     if(err){
