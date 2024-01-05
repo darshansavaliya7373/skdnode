2eaee8acef803fec5554bd77c8f813036543c081
darshansavaliya7373 <darshansavaliya7373@gmail.com>
2024-01-05 21:06:15 +0530

    donne

--- router/user.router.js
+++ router/user.router.js
@@ -1,6 +1,7 @@
 
 var express = require("express")
 var router = express.Router()
+var authentication = require('../middleware/user.middleware.js')
 const{
     login,
     addcoil,
@@ -20,20 +21,20 @@ const{
 }=require('../controller/user.controller.js')
 
 router.post('/login',login)
-router.post('/addcoil',addcoil)
-router.get('/todayaddcoil',todayaddcoil)
-router.post('/coilenter/:id',coilenter)
-router.post('/singlecoilupdate/:id/:coil',singlecoilupdate)
-router.get('/allcoil',allcoil)
-router.post('/updatepalate',updatepalate)
-router.get('/emptypalateno',emptypalateno)
-router.delete('/deletesinglecoil/:id',deleteSingleCoil)
-router.get('/operatorwithcoil',operatorwithcoil)
-router.post('/chalan',chalan)
-router.get('/allchalans',allchalans)
-router.get('/generateBill/:id',generateBill)
-router.get('/allmm',allmm)
-router.get('/profile',(req,res)=>{
+router.post('/addcoil',authentication,addcoil)
+router.get('/todayaddcoil',authentication,todayaddcoil)
+router.post('/coilenter/:id',authentication,coilenter)
+router.post('/singlecoilupdate/:id/:coil',authentication,singlecoilupdate)
+router.get('/allcoil',authentication,allcoil)
+router.post('/updatepalate',authentication,updatepalate)
+router.get('/emptypalateno',authentication,emptypalateno)
+router.delete('/deletesinglecoil/:id',authentication,deleteSingleCoil)
+router.get('/operatorwithcoil',authentication,operatorwithcoil)
+router.post('/chalan',authentication,chalan)
+router.get('/allchalans',authentication,allchalans)
+router.get('/generateBill/:id',authentication,generateBill)
+router.get('/allmm',authentication,allmm)
+router.get('/profile',authentication,(req,res)=>{
     res.json(req.user)
 })
 
